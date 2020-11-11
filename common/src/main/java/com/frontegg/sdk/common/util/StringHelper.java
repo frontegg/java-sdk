8963efd8e0799f782e7c17fb58f6827b03893454
ARTHUR <artur.hovhannisyan@wcfsmart.com>
2020-11-11 20:18:39 +0400

    chore: configuration provider implementations

--- common/src/main/java/com/frontegg/sdk/common/util/StringHelper.java
+++ common/src/main/java/com/frontegg/sdk/common/util/StringHelper.java
@@ -3,6 +3,10 @@ package com.frontegg.sdk.common.util;
 public class StringHelper {
 
     public static boolean isBlank(String val) {
-        return val == null || val.length() == 0;
+        return isBlank(val, false);
+    }
+
+    public static boolean isBlank(String val, boolean trim) {
+        return val == null || (trim ? val.trim().length() == 0 : val.length() == 0);
     }
 }
--- config/src/main/java/com/frontegg/sdk/config/FronteggUrlConfig.java
+++ config/src/main/java/com/frontegg/sdk/config/FronteggUrlConfig.java
@@ -1,9 +1,26 @@
 package com.frontegg.sdk.config;
 
 public class FronteggUrlConfig {
-    private static final String DEFAULT_BASE_URL = "https://api.frontegg.com";
-    private static final String DEFAULT_AUTH_SERVICE_URL = DEFAULT_BASE_URL + "/auth/vendor";
-    private static final String DEFAULT_AUDIT_URL = DEFAULT_BASE_URL + "/audits";
+    public static final String DEFAULT_BASE_URL = "https://api.frontegg.com";
+    public static final String DEFAULT_AUTH_SERVICE_URL = DEFAULT_BASE_URL + "/vendors/auth/token";
+    public static final String DEFAULT_AUDIT_URL = DEFAULT_BASE_URL + "/audits/";
+    public static final String DEFAULT_NOTIFICATION_SERVICE_URL = DEFAULT_BASE_URL + "/notification/";
+    public static final String DEFAULT_TENANT_SERVICE_URL = DEFAULT_BASE_URL + "/tenants/";
+    public static final String DEFAULT_METADATA_SERVICE_URL = DEFAULT_BASE_URL + "/metadata/";
+    public static final String DEFAULT_TEAM_SERVICE_URL = DEFAULT_BASE_URL + "/team";
+    public static final String DEFAULT_EVENT_SERVICE_URL = DEFAULT_BASE_URL + "/event";
+    public static final String DEFAULT_IDENTITY_SERVICE_URL = DEFAULT_BASE_URL + "/identity";
+
+
+    public static final String PROPERTY_KEY_DEFAULT_BASE_URL = "baseUrl";
+    public static final String PROPERTY_KEY_DEFAULT_AUTH_SERVICE = "authenticationService";
+    public static final String PROPERTY_KEY_DEFAULT_AUDIT_SERVICE = "auditsService";
+    public static final String PROPERTY_KEY_DEFAULT_NOTIFICATION_SERVICE = "notificationService";
+    public static final String PROPERTY_KEY_DEFAULT_TENANT_SERVICE = "tenantsService";
+    public static final String PROPERTY_KEY_DEFAULT_METADATA_SERVICE = "metadataService";
+    public static final String PROPERTY_KEY_DEFAULT_TEAM_SERVICE = "teamService";
+    public static final String PROPERTY_KEY_DEFAULT_EVENT_SERVICE = "eventService";
+    public static final String PROPERTY_KEY_DEFAULT_IDENTITY_SERVICE = "identityService";
 
     private String baseUrl;
     private String authenticationService;
@@ -15,39 +32,17 @@ public class FronteggUrlConfig {
     private String eventService;
     private String identityService;
 
-    /*
-
-
-    export let baseUrl = process.env.FRONTEGG_API_GATEWAY_URL || "https://api.frontegg.com/";
-if (baseUrl.endsWith('/')) {
-  baseUrl = baseUrl.slice(0, -1);
-}
-
-// tslint:disable-next-line:no-namespace
-export namespace config {
-  // tslint:disable-next-line:class-name
-  export class urls {
-    public static authenticationService = process.env.FRONTEGG_AUTHENTICATION_SERVICE_URL || `${baseUrl}/vendors/auth/token`;
-    public static auditsService = process.env.FRONTEGG_AUDITS_SERVICE_URL || `${baseUrl}/audits/`;
-    public static notificationService = process.env.FRONTEGG_NOTIFICATION_SERVICE_URL || `${baseUrl}/notification/`;
-    public static tenantsService = process.env.FRONTEGG_TENANTS_SERVICE_URL || `${baseUrl}/tenants/`;
-    public static metadataService = process.env.FRONTEGG_METADATA_SERVICE_URL || `${baseUrl}/metadata/`;
-    public static teamService = process.env.FRONTEGG_TEAM_MANAGEMENT_SERVICE_URL || `${baseUrl}/team`;
-    public static eventService = process.env.FRONTEGG_EVENT_SERVICE_URL || `${baseUrl}/event`;
-    public static identityService = process.env.FRONTEGG_IDENTITY_SERVICE_URL || `${baseUrl}/identity`;
-  }
-}
-
-
-     */
-
     public static FronteggUrlConfig createDefaults() {
         FronteggUrlConfig urlConfig = new FronteggUrlConfig();
         urlConfig.baseUrl = DEFAULT_BASE_URL;
         urlConfig.authenticationService = DEFAULT_AUTH_SERVICE_URL;
-
-        //TODO comp[lete
-
+        urlConfig.auditsService = DEFAULT_AUDIT_URL;
+        urlConfig.notificationService = DEFAULT_NOTIFICATION_SERVICE_URL;
+        urlConfig.tenantsService = DEFAULT_TENANT_SERVICE_URL;
+        urlConfig.metadataService = DEFAULT_METADATA_SERVICE_URL;
+        urlConfig.teamService = DEFAULT_TEAM_SERVICE_URL;
+        urlConfig.eventService = DEFAULT_EVENT_SERVICE_URL;
+        urlConfig.identityService = DEFAULT_IDENTITY_SERVICE_URL;
         return urlConfig;
     }
 
@@ -55,35 +50,71 @@ export namespace config {
         return baseUrl;
     }
 
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
     public String getAuthenticationService() {
         return authenticationService;
+    }
+
+    public void setAuthenticationService(String authenticationService) {
+        this.authenticationService = authenticationService;
     }
 
     public String getAuditsService() {
         return auditsService;
     }
 
+    public void setAuditsService(String auditsService) {
+        this.auditsService = auditsService;
+    }
+
     public String getNotificationService() {
         return notificationService;
+    }
+
+    public void setNotificationService(String notificationService) {
+        this.notificationService = notificationService;
     }
 
     public String getTenantsService() {
         return tenantsService;
     }
 
+    public void setTenantsService(String tenantsService) {
+        this.tenantsService = tenantsService;
+    }
+
     public String getMetadataService() {
         return metadataService;
+    }
+
+    public void setMetadataService(String metadataService) {
+        this.metadataService = metadataService;
     }
 
     public String getTeamService() {
         return teamService;
     }
 
+    public void setTeamService(String teamService) {
+        this.teamService = teamService;
+    }
+
     public String getEventService() {
         return eventService;
     }
 
+    public void setEventService(String eventService) {
+        this.eventService = eventService;
+    }
+
     public String getIdentityService() {
         return identityService;
+    }
+
+    public void setIdentityService(String identityService) {
+        this.identityService = identityService;
     }
 }
